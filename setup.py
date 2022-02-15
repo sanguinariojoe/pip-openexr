8e5218b06e1ebc06b4f7017038045c850da702fd
Jose Luis Cercos-Pita <jlcercos@gmail.com>
2022-02-15 17:10:30 +0100

    Check if we shall use the new libOpenEXR or the old set of libraries

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from distutils.extension import Extension
 from distutils.command.build_py import build_py as _build_py
 
 from os import system
+import platform
 
 from distutils.core import setup, Extension
 
@@ -18,7 +19,14 @@ page:
 https://github.com/sanguinariojoe/pip-openexr/issues
 """
 
-version = "1.3.6"
+print("Looking for libOpenEXR...")
+if platform.system() == "Linux" and system("ldconfig -p | grep libOpenEXR"):
+    # There is no libOpenEXR, probably an old version of OpenEXR
+    libraries=['Iex', 'Half', 'Imath', 'IlmImf', 'z']
+else:
+    libraries=['Iex', 'OpenEXR', 'z']
+
+version = "1.3.7"
 setup(name='OpenEXR',
   author = 'James Bowman',
   author_email = 'jamesb@excamera.com',
@@ -35,8 +43,10 @@ setup(name='OpenEXR',
                             '/usr/include/Imath',
                             '/usr/local/include/Imath',
                             '/opt/local/include/Imath'],
-              library_dirs=['/usr/local/lib', '/opt/local/lib'],
-              libraries=['Iex', 'OpenEXR', 'z'],
+              library_dirs=['/usr/lib',
+                            '/usr/local/lib',
+                            '/opt/local/lib'],
+              libraries=libraries,
               extra_compile_args=['-g', '-DVERSION="%s"' % version])
   ],
   py_modules=['Imath'],
